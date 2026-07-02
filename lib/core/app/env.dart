6f172dacdf38e37dfaf2f3562a8e1554f83f109e
SayuZX <pixelraihan77@gmail.com>
2026-07-02 16:31:33 +0700

    feat(env): add configurable sub indo API base url

--- lib/core/app/env.dart
+++ lib/core/app/env.dart
@@ -1,5 +1,11 @@
 class KumaAnimeEnvironment {
   static const String simklClientId = String.fromEnvironment("SIMKL_CLIENT_ID");
-  static const String simklClientSecret = String.fromEnvironment("SIMKL_CLIENT_SECRET");
-  static const String commentumApiUrl = String.fromEnvironment("COMMENTUM_API_URL");
+  static const String simklClientSecret =
+      String.fromEnvironment("SIMKL_CLIENT_SECRET");
+  static const String commentumApiUrl =
+      String.fromEnvironment("COMMENTUM_API_URL");
+  static const String subIndoApiUrl = String.fromEnvironment(
+    "SUBINDO_API_URL",
+    defaultValue: "https://wajik-anime-api.vercel.app",
+  );
 }
