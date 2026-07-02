cbfe19feb9311b2b394f472208cb10042eaad7f7
SayuZX <pixelraihan77@gmail.com>
2026-07-02 14:42:16 +0700

    refactor(kwik): rename package imports to kumaanime

--- lib/core/anime/extractors/kwik.dart
+++ lib/core/anime/extractors/kwik.dart
@@ -1,5 +1,5 @@
-import 'package:animestream/core/anime/extractors/type.dart';
-import 'package:animestream/core/anime/providers/types.dart';
+import 'package:kumaanime/core/anime/extractors/type.dart';
+import 'package:kumaanime/core/anime/providers/types.dart';
 import 'package:http/http.dart';
 import 'package:html/parser.dart' as html;
 
