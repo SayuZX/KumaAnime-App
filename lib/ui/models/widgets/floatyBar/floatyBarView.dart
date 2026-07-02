8f354b9659f257a29a720515bc17308a7765e8c3
SayuZX <pixelraihan77@gmail.com>
2026-07-02 14:43:02 +0700

    refactor(floatyBarView): rename package imports to kumaanime

--- lib/ui/models/widgets/floatyBar/floatyBarView.dart
+++ lib/ui/models/widgets/floatyBar/floatyBarView.dart
@@ -1,4 +1,4 @@
-import 'package:animestream/ui/models/widgets/floatyBar/controller.dart';
+import 'package:kumaanime/ui/models/widgets/floatyBar/controller.dart';
 import 'package:flutter/material.dart';
 
 class FloatyBarView extends StatefulWidget {
