531165d5e2123ae48561a97882fe2720410d714f
SayuZX <pixelraihan77@gmail.com>
2026-07-02 14:42:29 +0700

    refactor(hive): rename default hive box to kumaanime

--- lib/core/data/hive.dart
+++ lib/core/data/hive.dart
@@ -1,8 +1,8 @@
-import 'package:animestream/core/app/logging.dart';
-import 'package:animestream/core/commons/enums/hiveEnums.dart';
+import 'package:kumaanime/core/app/logging.dart';
+import 'package:kumaanime/core/commons/enums/hiveEnums.dart';
 import 'package:hive/hive.dart';
 
-Future<dynamic>? getVal(HiveKey itemKey, {HiveBox boxName = HiveBox.animestream}) async {
+Future<dynamic>? getVal(HiveKey itemKey, {HiveBox boxName = HiveBox.kumaanime}) async {
   var box = await Hive.openBox(boxName.boxName);
   if (!box.isOpen) {
     box = await Hive.openBox(boxName.boxName);
@@ -12,7 +12,7 @@ Future<dynamic>? getVal(HiveKey itemKey, {HiveBox boxName = HiveBox.animestream}
   return vals;
 }
 
-Future<void> storeVal(HiveKey itemKey, dynamic val, {HiveBox boxName = HiveBox.animestream}) async {
+Future<void> storeVal(HiveKey itemKey, dynamic val, {HiveBox boxName = HiveBox.kumaanime}) async {
   try {
     var box = await Hive.openBox(boxName.boxName);
     if (!box.isOpen) {
