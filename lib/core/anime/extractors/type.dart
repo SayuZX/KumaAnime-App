e4805a7a9e4267d62e0cfa0bb4b37a151c3010e8
SayuZX <pixelraihan77@gmail.com>
2026-07-02 14:42:17 +0700

    refactor(type): rename package imports to kumaanime

--- lib/core/anime/extractors/type.dart
+++ lib/core/anime/extractors/type.dart
@@ -1,4 +1,4 @@
-import 'package:animestream/core/anime/providers/types.dart';
+import 'package:kumaanime/core/anime/providers/types.dart';
 
 abstract class AnimeExtractor {
   Future<List<VideoStream>> extract(String streamUrl);
