694794dca76bb6ed05b30cc8f4a83d05208fcee2
SayuZX <pixelraihan77@gmail.com>
2026-07-02 14:42:23 +0700

    refactor(runtimeDatas): rename animestream identifiers to kumaanime

--- lib/core/app/runtimeDatas.dart
+++ lib/core/app/runtimeDatas.dart
@@ -1,6 +1,6 @@
-import 'package:animestream/core/data/types.dart';
-import 'package:animestream/core/database/anilist/types.dart';
-import 'package:animestream/ui/theme/types.dart';
+import 'package:kumaanime/core/data/types.dart';
+import 'package:kumaanime/core/database/anilist/types.dart';
+import 'package:kumaanime/ui/theme/types.dart';
 // import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 //saved anilist data
@@ -13,6 +13,6 @@ SettingsModal? currentUserSettings;
 UserPreferencesModal? userPreferences;
 
 //saved theme
-late AnimeStreamTheme appTheme;
+late KumaAnimeTheme appTheme;
 
 late String animeOnsenToken;
