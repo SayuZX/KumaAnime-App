748254014d47098d4e591f743697cf41fa34e0c9
SayuZX <pixelraihan77@gmail.com>
2026-07-02 14:42:22 +0700

    refactor(env): rename AnimeStreamEnvironment to KumaAnimeEnvironment

--- lib/core/app/env.dart
+++ lib/core/app/env.dart
@@ -1,4 +1,4 @@
-class AnimeStreamEnvironment {
+class KumaAnimeEnvironment {
   static const String simklClientId = String.fromEnvironment("SIMKL_CLIENT_ID");
   static const String simklClientSecret = String.fromEnvironment("SIMKL_CLIENT_SECRET");
   static const String commentumApiUrl = String.fromEnvironment("COMMENTUM_API_URL");
