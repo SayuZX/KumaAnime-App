cddcb7ce3b03fd5eea492bbb5f96bf9e9cd200d4
SayuZX <pixelraihan77@gmail.com>
2026-07-02 14:43:09 +0700

    refactor(controls): rename package imports to kumaanime

--- lib/ui/models/widgets/player/controls.dart
+++ lib/ui/models/widgets/player/controls.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
-import 'package:animestream/ui/models/widgets/player/desktopControls/desktopControls.dart';
-import 'package:animestream/ui/models/widgets/player/mobileControls/mobileControls.dart';
+import 'package:kumaanime/ui/models/widgets/player/desktopControls/desktopControls.dart';
+import 'package:kumaanime/ui/models/widgets/player/mobileControls/mobileControls.dart';
 import 'package:flutter/material.dart';
 
 /// A wrapper class for selecting platform-specific controls
