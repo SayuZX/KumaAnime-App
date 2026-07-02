5e8350b3210eadab9cfddcfe2670dd64db6e605c
SayuZX <pixelraihan77@gmail.com>
2026-07-02 14:42:55 +0700

    refactor(snackBar): rename method channel to kumaanime.app/utils

--- lib/ui/models/snackBar.dart
+++ lib/ui/models/snackBar.dart
@@ -1,13 +1,13 @@
 import "dart:io";
 
-import "package:animestream/main.dart";
+import "package:kumaanime/main.dart";
 import "package:flutter/material.dart";
 import "package:flutter/services.dart";
 
 void floatingSnackBar(String message, {int? duration, bool waitForPreviousToFinish = false}) {
   final isWindows = Platform.isWindows;
-  if (!waitForPreviousToFinish) AnimeStream.snackbarKey.currentState?.removeCurrentSnackBar();
-  AnimeStream.snackbarKey.currentState?.showSnackBar(
+  if (!waitForPreviousToFinish) KumaAnime.snackbarKey.currentState?.removeCurrentSnackBar();
+  KumaAnime.snackbarKey.currentState?.showSnackBar(
       SnackBar(
         content: Center(
           child: Text(message, style: TextStyle(fontFamily: "NotoSans", color: Colors.white, fontSize: 14)),
@@ -17,13 +17,13 @@ void floatingSnackBar(String message, {int? duration, bool waitForPreviousToFini
         behavior: SnackBarBehavior.floating,
         dismissDirection: DismissDirection.down,
         margin: isWindows ? null : EdgeInsets.only(bottom: 40, left: 20, right: 20),
-        width: isWindows ? MediaQuery.of(AnimeStream.snackbarKey.currentState!.context).size.width / 5 : null,
+        width: isWindows ? MediaQuery.of(KumaAnime.snackbarKey.currentState!.context).size.width / 5 : null,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
       ),
     );
 }
 
 void showToast(String message) async {
-  final platform = MethodChannel('animestream.app/utils');
+  final platform = MethodChannel('kumaanime.app/utils');
   await platform.invokeMethod("showToast", {'message': message});
 }
