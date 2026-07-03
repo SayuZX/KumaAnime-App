9df8f39b8857258a5f29c01e610feab3693360fb
SayuZX <pixelraihan77@gmail.com>
2026-07-03 08:00:43 +0700

    fix(ui): normal bars when leaving subtitle settings

--- lib/ui/pages/settingPages/subtitle.dart
+++ lib/ui/pages/settingPages/subtitle.dart
@@ -47,7 +47,8 @@ class _SubtitleSettingPageState extends State<SubtitleSettingPage> {
     } else {
       SystemChrome.setPreferredOrientations(
           [DeviceOrientation.portraitUp, DeviceOrientation.landscapeRight, DeviceOrientation.landscapeLeft]);
-      SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+          overlays: [SystemUiOverlay.top, SystemUiOverlay.bottom]);
     }
     super.dispose();
   }
