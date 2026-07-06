91cabfc591faa258ee34ad4a535fd1dde76b99af
SayuZX <pixelraihan77@gmail.com>
2026-07-07 00:34:13 +0700

    feat(ui): add search button to global buildHeader widget

--- lib/ui/models/widgets/header.dart
+++ lib/ui/models/widgets/header.dart
@@ -1,10 +1,11 @@
 import 'package:kumaanime/core/app/runtimeDatas.dart';
 import 'package:kumaanime/ui/pages/settings.dart';
+import 'package:kumaanime/ui/pages/search.dart';
 import 'package:flutter/material.dart';
 
 Container buildHeader(String title, BuildContext context, {void Function()? afterNavigation}) {
   return Container(
-    padding: EdgeInsets.only(left: 20, top: 10, right: 20, bottom: 20),
+    padding: const EdgeInsets.only(left: 20, top: 10, right: 20, bottom: 20),
     child: Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
@@ -20,7 +21,20 @@ Container buildHeader(String title, BuildContext context, {void Function()? afte
           children: [
             IconButton(
               onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => SettingsPage())).then((val) {
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Search())).then((val) {
+                  if (afterNavigation != null) afterNavigation();
+                });
+              },
+              icon: Icon(
+                Icons.search_rounded,
+                color: appTheme.textMainColor,
+                size: 32,
+              ),
+            ),
+            const SizedBox(width: 8),
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) => const SettingsPage())).then((val) {
                   if (afterNavigation != null) afterNavigation();
                 });
               },
