97bb1d78d3fd657c43e73c0c19ed53e816510da6
SayuZX <pixelraihan77@gmail.com>
2026-07-20 18:35:43 +0700

    feat(version): add buildNumber and buildDate formatting in AppVersion

--- lib/core/app/version.dart
+++ lib/core/app/version.dart
@@ -4,14 +4,19 @@ import 'package:package_info_plus/package_info_plus.dart';
 class AppVersion {
   /// App's version
   late final String version;
+  late final String buildNumber;
+  late final String buildDate;
 
   AppVersion._();
 
   static AppVersion? _instance;
 
   static AppVersion get instance {
-    if (_instance != null) return _instance!;
-    else throw Exception("AppVersion has not yet been initialised.");
+    if (_instance != null) {
+      return _instance!;
+    } else {
+      throw Exception("AppVersion has not yet been initialised.");
+    }
   }
 
   /// Initialises the instance, Loads version
@@ -19,7 +24,18 @@ class AppVersion {
     final info = await PackageInfo.fromPlatform();
     final instance = AppVersion._();
     instance.version = info.version;
+    instance.buildNumber = info.buildNumber;
+    instance.buildDate = _formatBuildDate(info.buildNumber);
     _instance = instance;
+  }
+
+  static String _formatBuildDate(String buildNum) {
+    if (buildNum.length >= 8 && RegExp(r'^\d{8}').hasMatch(buildNum)) {
+      final s = buildNum.substring(0, 8);
+      return "${s.substring(0, 4)}-${s.substring(4, 6)}-${s.substring(6, 8)}";
+    }
+    final now = DateTime.now();
+    return "${now.year}-${now.month.toString().padLeft(2, '0')}-${now.day.toString().padLeft(2, '0')}";
   }
 
   /// Codename
